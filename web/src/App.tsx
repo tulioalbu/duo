830b593625572695cc4625b7ad49f66adf03cf66
Túlio Albuquerque <tulio.albuquerque@equipeblue.com.br>
2023-07-20 18:07:38 -0300

    evoluções web

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -78,47 +78,46 @@ function App() {
                 </div>
               </div>
 
-                <div className='flex gap-6'>
-                  <div className="flex flex-col gap-2">
-                    <label htmlFor='weekDays'>Quando costuma jogar?</label>
-                  </div>
+              <div className='flex gap-6'>
+                <div className="flex flex-col gap-2">
+                  <label htmlFor='weekDays'>Quando costuma jogar?</label>
 
                   <div className='grid grid-cols-4 gap-2'>
-                    <button title='Domingo' className='w-8 h-8 rounded bg-zinc-900'>D</button>
-                    <button title='Segunda' className='w-8 h-8 rounded bg-zinc-900'>S</button>
-                    <button title='Terça' className='w-8 h-8 rounded bg-zinc-900'>T</button>
-                    <button title='Quarta' className='w-8 h-8 rounded bg-zinc-900'>Q</button>
-                    <button title='Quinta' className='w-8 h-8 rounded bg-zinc-900'>Q</button>
-                    <button title='Sexta' className='w-8 h-8 rounded bg-zinc-900'>S</button>
-                    <button title='Sábado' className='w-8 h-8 rounded bg-zinc-900'>S</button>
+                    <button title='Domingo' className='w-9 h-9 rounded bg-zinc-900'>D</button>
+                    <button title='Segunda' className='w-9 h-9 rounded bg-zinc-900'>S</button>
+                    <button title='Terça' className='w-9 h-9 rounded bg-zinc-900'>T</button>
+                    <button title='Quarta' className='w-9 h-9 rounded bg-zinc-900'>Q</button>
+                    <button title='Quinta' className='w-9 h-9 rounded bg-zinc-900'>Q</button>
+                    <button title='Sexta' className='w-9 h-9 rounded bg-zinc-900'>S</button>
+                    <button title='Sábado' className='w-9 h-9 rounded bg-zinc-900'>S</button>
 
-                  </div>
-
-                  <div className="flex flex-col gap-2 flex-1">
-                    <label htmlFor='hourStart'>Qual o horário do dia?</label>
                   </div>
                 </div>
 
-                <div className='grid grid-cols-2 gap-2'>
-                  <Input id='hourStart' type='time' placeholder='De' />
-                  <Input id='hourEnd' type='time' placeholder='Até' />
-                </div>
-
-                <div className='flex flex-col gap-2 text-sm'>
-                  <Input type='checkbox'/>
-                  Costumo me conectar ao chat de voz
+                <div className="flex flex-col gap-2 flex-1">
+                  <label htmlFor='hourStart'>Qual o horário do dia?</label>
+                  <div className='grid grid-cols-2 gap-2'>
+                    <Input id='hourStart' type='time' placeholder='De' />
+                    <Input id='hourEnd' type='time' placeholder='Até' />
                   </div>
+                </div>
+              </div>
 
-                  <footer className='mt-4 flex justify-end gap-4'>
-                    <button className='bg-zinc-500 rounded px-5 h-12 rounded-md font-semibold hover:bg-zinc-600'>Cancelar</button>
-                    <button type="submit" className='bg-violet-500 rounded-md px-5 h-12 text-sm font-semibold flex items-center gap-3 hover:bg-violet-600'>
-                    <GameController className='w-6 h-6' />
-                    Encontrar duo
-                    </button>
-                  </footer>
-    
+
+              <div className='mt-2 flex gap-2 text-sm'>
+                <Input type='checkbox' />
+                Costumo me conectar ao chat de voz
+              </div>
+
+              <footer className='mt-4 flex justify-end gap-4'>
+                <Dialog.Close type="button" className='bg-zinc-500 rounded px-5 h-12 rounded-md font-semibold hover:bg-zinc-600'>Cancelar</Dialog.Close>
+                <button type="submit" className='bg-violet-500 rounded-md px-5 h-12 text-sm font-semibold flex items-center gap-3 hover:bg-violet-600'>
+                  <GameController className='w-6 h-6' />
+                  Encontrar duo
+                </button>
+              </footer>
+
             </form>
-
           </Dialog.Content>
         </Dialog.Portal>
       </Dialog.Root>
